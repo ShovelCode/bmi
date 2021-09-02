fc516c914aef7423e3d6bf3d3068e6ed26edce26
Christopher Ray <vde435@gmail.com>
2021-09-02 02:21:31 -0500

    Update
    Should be outputting metric body mass index.

--- bmi.c
+++ bmi.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main (void){
+	float weight; //kilograms
+	float height; //meters
+	float bmi;
 	printf("%s", "Enter weight in kilograms: ");
-	int weight;
-	scanf("%d", &weight);
-	printf("Weight: %d", weight);
+	
+	scanf("%f", &weight);
+	printf("Enter height in meters: ");
+	scanf("%f", &height);
+	bmi = weight / (height * height);
+	printf("bmi: %f", bmi);
 }
